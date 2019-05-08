04b40c16cbea1180f4fce06f18bb0f3d8cfcc80f
Lapek-Komputer\Lapek <wgabel34@gmail.com>
2019-05-08 23:10:32 +0200

    add packages folder

---
+++ Assets/WgUnityPackages/InventoryForUnity/IInventory.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WgUnityPackages.IInventoryForUnity
+{
+    public interface IInventory
+    {
+        void Put<T> ( T instance ) where T: class;
+        T GetFirstOrNull<T>() where T : class;
+        T Get<T> ( int id ) where T : class;
+        void Destroy();
+    }
+}
+
